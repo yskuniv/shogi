84df154768f0d391c46b72576fe55ded21b9a7e2
ysk_univ <ysk.univ.1007@gmail.com>
2022-09-19 20:30:11 +0900

    [.eslintrc.cjs] fix format

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,9 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:@typescript-eslint/recommended-requiring-type-checking'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
